ad3240a0172e588c0c978c7621f3ef63a7a7641c
Mikko Harju <mikko@taiste.fi>
2021-03-29 10:43:15 +0300

    Version 0.2.5-SNAPSHOT
    Updated dependencies and fixed typo in URLs.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject nilenso/mailgun "0.2.4-SNAPSHOT"
+(defproject nilenso/mailgun "0.2.5-SNAPSHOT"
   :description "Mailgun API in clojure"
   :url "http://github.com/nilenso/mailgun"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [clj-http "3.9.1"]
-                 [cheshire "5.8.1"]])
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [clj-http "3.12.1"]
+                 [cheshire "5.10.0"]])
